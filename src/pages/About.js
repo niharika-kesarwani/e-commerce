d0777e259b3db830f82e365f48cd44e76604c0e6
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-04-17 16:04:46 +0530

    handle undefined products in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import { useProducts } from "../context/product-context";
 import ProductCard from "../components/ProductCard";
+import { Error } from "./Error";
 import "../styles/About.css"
 
 export function About () {
@@ -11,8 +12,10 @@ export function About () {
 
     return (
         <div>
-            <h2>About</h2>
-            <ProductCard {...visitedProduct} visited about/>
+            {visitedProduct === undefined ? <Error/> : <div>
+                <h2>About</h2>
+                <ProductCard {...visitedProduct} visited about/>
+            </div>}
         </div>
     )
 }
