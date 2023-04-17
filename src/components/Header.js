33802008340ae4ee5e6d637b5bb8c1be171f67b9
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-04-17 16:33:15 +0530

    added navlink to product listing heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const navLinkStyle = {
 export function Header() {
     return (
     <div>
-        <h1>Product Listing Page</h1>
+        <h1><NavLink to="/">Product Listing Page</NavLink></h1>
         <nav>
             <NavLink style={navLinkStyle} to="/cart" >My Cart</NavLink>
             <NavLink style={navLinkStyle} to="/wishlist">My Wishlist</NavLink>
