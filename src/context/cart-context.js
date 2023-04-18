1d3f69f7bc91c3e60ad781e10c611733c9ce3dc1
Niharika Kesarwani <45658446+niharika-kesarwani@users.noreply.github.com>
2023-04-19 01:16:57 +0530

    handled loading and manual routing states (#1)

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -1,20 +1,26 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react";
 import { useProducts } from "./product-context";
 
 export const CartContext = createContext();
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
+  const { productsArray } = useProducts();
+  const [cartArray, setCartArray] = useState([]);
 
-    const {productsArray} = useProducts();
-    const [cartArray, setCartArray] = useState([]);
+  const addToCart = (addItemId) =>
+    setCartArray([
+      productsArray.find(({ id }) => id === addItemId),
+      ...cartArray,
+    ]);
 
-    const addToCart = addItemId => setCartArray([productsArray.find(({id}) => id === addItemId), ...cartArray]);
+  const removeFromCart = (removeItemId) =>
+    setCartArray(cartArray.filter(({ id }) => id !== removeItemId));
 
-    const removeFromCart = removeItemId => setCartArray(cartArray.filter(({id}) => id !== removeItemId))
-
-    return <CartContext.Provider value={{cartArray, addToCart, removeFromCart}}>
-        {children}
+  return (
+    <CartContext.Provider value={{ cartArray, addToCart, removeFromCart }}>
+      {children}
     </CartContext.Provider>
-}
+  );
+};
 
 export const useCart = () => useContext(CartContext);
--- src/context/product-context.js
+++ src/context/product-context.js
@@ -5,21 +5,25 @@ export const ProductContext = createContext();
 
 export const ProductProvider = ({children}) => {
 
-    const [productsArray, setProductsArray] = useState([])
+    const [productsArray, setProductsArray] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
 
     const getProductData = async () => {
         try {
+            setIsLoading(true);
             const {status, data: {products}} = await fakeFetch("https://example.com/api/products");
+            setIsLoading(false);
             if (status === 200) {
                 setProductsArray(products);
             }
         } catch (err) {
+            setIsLoading(false);
             console.error(err)
         }
     }
 
     useEffect(() => getProductData, [])
-    return (<ProductContext.Provider value={{productsArray}}>
+    return (<ProductContext.Provider value={{productsArray, isLoading}}>
         {children}
     </ProductContext.Provider>)
 }
